b7c7c3d5095502e2a0bc7a700013aeae6551ecff
Richard <charichard09@gmail.com>
2023-02-10 10:22:27 -0800

    add basic project setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { getByTestId } from '@testing-library/react';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+    </React.Fragment>  
+  );
+}
+
+export default App;
