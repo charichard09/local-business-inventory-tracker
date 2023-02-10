a88e917412ace50a6e2c0864fe67d8fc441841f5
Richard <charichard09@gmail.com>
2023-02-10 10:27:18 -0800

    add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import { getByTestId } from '@testing-library/react';
+import Header from './Header';
 import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
     </React.Fragment>  
   );
 }
